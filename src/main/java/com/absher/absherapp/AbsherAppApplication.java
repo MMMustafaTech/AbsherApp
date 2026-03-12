daf2cf51295c8ed2a62c8714057099517a7d600e
Mohammed Mustafa <mohammedmaher6000@gmail.com>
2026-03-12 08:09:28 +0300

    initial spring boot backend setup

---
+++ src/main/java/com/absher/absherapp/AbsherAppApplication.java
@@ -0,0 +1,13 @@
+package com.absher.absherapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbsherAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AbsherAppApplication.class, args);
+    }
+
+}
